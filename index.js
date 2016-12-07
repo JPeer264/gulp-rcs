52f6a5e955fd95bb4af247825e96f24cf2baae3e
JPeer264 <jan.oster94@gmail.com>
2016-12-07 21:24:16 +0100

    updated:  * index  * tests added:  * todos

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 'use strict';
 
 const path = require('path');
+const rcs = require('rcs-core');
 const through = require('through2');
 
-module.exports = function (file, opt) {
-    if (!file) {
-        throw new Error('gulp-concat: Missing file option');
-    }
-
+module.exports = opt => {
     opt = opt || {};
 
-    console.log(file, opt)
+    opt.css = opt.css || ['css'];
+
+    return through.obj(function (file, enc, cb) {
+        if (file.isNull()) {
+            return cb(null, file);
+        }
+
+        // @todo check if file.relative exists in opt.css
+        if (path.extname(file.relative) === '.css') {
+            // @todo save all selectors into selectorLibrary from rcs-core
+            console.log(file.relative)
+        }
+
+        // @todo rename this file based on the selectorLibrary from rcs-core
+
+        this.push(file);
+
+        cb();
+    });
 }
--- test/rcs.spec.js
+++ test/rcs.spec.js
@@ -1,10 +1,22 @@
-const fs     = require('fs');
-const rcs    = require('../');
-const path   = require('path');
-const expect = require('chai').expect;
+const fs   = require('fs-extra');
+const rcs  = require('../');
+const path = require('path');
+const gulp = require('gulp');
+
+const testFiles = './test/files';
+const fixtures = testFiles + '/fixtures';
+const temp = testFiles + '/tmp';
 
 describe('gulp-rcs', () => {
-    it('should rename files', done => {
-        done();
+    after(() => {
+        fs.removeSync(temp);
+    });
+
+    it('should rename files', () => {
+        gulp.src([fixtures + '/**/*.css', fixtures + '/**/*.*'])
+            .pipe(rcs())
+            .pipe(gulp.dest(temp));
+
+        // @todo set expect tests here
     });
 });
