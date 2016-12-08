f4dae7f83cdd7cfc0f689d55c6d3b4de574a5d9e
JPeer264 <jan.oster94@gmail.com>
2016-12-08 10:31:50 +0100

    updated:  * replace function is now just called once  * throw error if file is stream

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const path = require('path');
-const rcs = require('rcs-core');
+const rcs     = require('rcs-core');
+const path    = require('path');
+const gutil   = require('gulp-util');
 const through = require('through2');
-const gutil = require('gulp-util');
 
 module.exports = opt => {
     opt = opt || {};
@@ -11,37 +11,41 @@ module.exports = opt => {
     opt.css = opt.css || ['css'];
 
     return through.obj(function (file, enc, cb) {
+        let replaceFunction = rcs.replace.file;
+
         if (file.isNull()) {
             return cb(null, file);
         }
 
+        if (file.isStream()) {
+            cb(new gutil.PluginError('gulp-rcs', 'Streaming not supported'));
+            return;
+        }
+
         // @todo check if file.relative exists in opt.css
         if (path.extname(file.relative) === '.css') {
-            rcs.replace.fileCss(file.path, (err, data) => {
-                let newFile = new gutil.File({
-                    cwd: file.cwd,
-                    base: file.base,
-                    path: file.path,
-                    contents: new Buffer(data.data)
-                });
-
-                this.push(newFile);
-
-                cb();
-            });
-        } else {
-            rcs.replace.file(file.path, (err, data) => {
-                let newFile = new gutil.File({
-                    cwd: file.cwd,
-                    base: file.base,
-                    path: file.path,
-                    contents: new Buffer(data.data)
-                });
-
-                this.push(newFile);
-
-                cb();
-            });
+            replaceFunction = rcs.replace.fileCss;
         }
+
+        // calling the replace function from rcs-core
+        replaceFunction(file.path, (err, data) => {
+            if (err) {
+                this.emit('error', new gutil.PluginError('gulp-rcs', err, {
+                    fileName: file.path,
+                    showProperties: false
+                }));
+            }
+
+            let newFile = new gutil.File({
+                cwd: file.cwd,
+                base: file.base,
+                path: file.path,
+                contents: new Buffer(data.data)
+            });
+
+            this.push(newFile);
+
+            cb();
+        });
     });
-}
+};
