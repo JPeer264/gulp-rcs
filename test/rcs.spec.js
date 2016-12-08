9890dd28e12f2fc205998db7f765593ff85bc4af
JPeer264 <jan.oster94@gmail.com>
2016-12-08 16:47:58 +0100

    fixed tests

--- test/rcs.spec.js
+++ test/rcs.spec.js
@@ -1,11 +1,9 @@
-const fs     = require('fs-extra');
-const rcs    = require('../');
+const fs      = require('fs-extra');
+const rcs     = require('../');
+const path    = require('path');
+const gutil   = require('gulp-util');
+const expect  = require('chai').expect;
 const rcsCore = require('rcs-core');
-const path   = require('path');
-const gulp   = require('gulp');
-const expect = require('chai').expect;
-const assert = require('stream-assert');
-const is = require('funsert');
 
 const testFiles = './test/files';
 const temp      = testFiles + '/tmp';
@@ -15,20 +13,45 @@ const fixtures  = testFiles + '/fixtures';
 describe('gulp-rcs', () => {
     beforeEach(() => {
         // reset counter and selectors for tests
+        rcsCore.selectorLibrary.excludes            = [];
         rcsCore.selectorLibrary.selectors           = {};
         rcsCore.selectorLibrary.compressedSelectors = {};
-        rcsCore.selectorLibrary.excludes            = [];
         rcsCore.nameGenerator.resetCountForTests();
     });
 
-    after(() => {
-        fs.removeSync(temp);
-    });
+    it('should rename all files', done => {
+        const stream = rcs();
 
-    it('should rename files', done => {
-        gulp.src([fixtures + '/style.css', fixtures + '/main.txt', fixtures + '/index.html'])
-            .pipe(rcs())
-            .pipe(gulp.dest(temp))
-            .pipe(assert.end(done))
+        stream.on('data', file => {
+            const contents = file.contents.toString();
+            const filename = path.basename(file.path);
+
+            expect(contents).to.equal(fs.readFileSync(results + '/' + filename, 'utf8'));
+        });
+
+        stream.on('end', done);
+
+        stream.write(new gutil.File({
+            cwd: __dirname,
+            base: fixtures,
+            path: fixtures + '/style.css',
+            contents: fs.readFileSync(fixtures + '/style.css')
+        }));
+
+        stream.write(new gutil.File({
+            cwd: __dirname,
+            base: fixtures,
+            path: fixtures + '/main.js',
+            contents: fs.readFileSync(fixtures + '/main.js')
+        }));
+
+        stream.write(new gutil.File({
+            cwd: __dirname,
+            base: fixtures,
+            path: fixtures + '/index.html',
+            contents: fs.readFileSync(fixtures + '/index.html')
+        }));
+
+        stream.end();
     });
 });
