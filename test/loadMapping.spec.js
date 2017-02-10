77edf3c3788cecc2e574dcb3658342f6c833f197
JPeer264 <jan.oster94@gmail.com>
2017-02-10 11:20:46 +0100

    add rcs.loadMapping

--- test/loadMapping.spec.js
+++ test/loadMapping.spec.js
@@ -5,16 +5,46 @@ const gutil   = require('gulp-util');
 const expect  = require('chai').expect;
 const rcsCore = require('rcs-core');
 
-describe.skip('loadMapping', () => {
-    it('should load mappings correctly', done => {
+describe.only('loadMapping', () => {
+    beforeEach(() => {
+        // reset counter and selectors for tests
+        rcsCore.selectorLibrary.excludes            = [];
+        rcsCore.selectorLibrary.selectors           = {};
+        rcsCore.selectorLibrary.compressedSelectors = {};
+        rcsCore.nameGenerator.resetCountForTests();
+    });
+
+    it('should load the mappings file correctly', done => {
         const stream = rcs();
 
-        rcs.loadMapping();
+        rcs.loadMapping('./test/files/renaming_map.json');
 
         stream.on('data', () => {});
 
         stream.on('end', () => {
-            // @todo write expects
+            expect(rcsCore.selectorLibrary.get('jp-block')).to.equal('a');
+            expect(rcsCore.selectorLibrary.get('.jp-pseudo')).to.equal('e');
+
+            done();
+        });
+
+        stream.end();
+    });
+
+    it('should load the mappings object correctly', done => {
+        const stream = rcs();
+
+        rcs.loadMapping({
+            '.jp-block': 'a',
+            '.jp-pseudo': 'e'
+        });
+
+        stream.on('data', () => {});
+
+        stream.on('end', () => {
+            expect(rcsCore.selectorLibrary.get('jp-block')).to.equal('a');
+            expect(rcsCore.selectorLibrary.get('.jp-pseudo')).to.equal('e');
+
             done();
         });
 
